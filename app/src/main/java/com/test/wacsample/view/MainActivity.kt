6d36e92b6b08d18e02f233424369c1903ce0beeb
Rijosh <rijosh.vk@enfintechnologies.com>
2023-11-11 06:36:56 +0530

    udates

--- app/src/main/java/com/test/wacsample/view/MainActivity.kt
+++ app/src/main/java/com/test/wacsample/view/MainActivity.kt
@@ -32,29 +32,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        val builder = AlertDialog.Builder(this@MainActivity)
-        builder.setMessage("No Internet Connection")
-        builder.setTitle("Attention!")
-        builder.setCancelable(false)
-        val alertDialog = builder.create()
-        val x = Snackbar.make(
-            binding.frameLayout,
-            R.string.no_internet_connection,
-            Snackbar.LENGTH_INDEFINITE
-        )
-        connectivityObserver = NetworkConnectivityObserver(applicationContext)
-        connectivityObserver.observe().onEach {
-            if (it == ConnectivityObserver.Status.Available) {
-                alertDialog.dismiss()
-                Log.i(TAG, "onCreate: status in $it")
-                //Snackbar.make(binding.frameLayout, "the connection is available", Snackbar.LENGTH_SHORT) .show()
-            } else if (it == ConnectivityObserver.Status.Lost) {
-                Log.i(TAG, "onCreate: status in $it")
-                alertDialog.show()
-            }
-
-        }.launchIn(lifecycleScope)
-
         beginTransaction(homeFragment)
         binding.bottomNavigationView.setOnItemSelectedListener {
             when (it.itemId) {
@@ -65,7 +42,6 @@ class MainActivity : AppCompatActivity() {
                     beginTransaction(CategoryFragment())
                 }
                 R.id.action_favorites -> {
-                    //val favoritesFragment = FavoriteFragment()//TODO(implement a better structure)
                     beginTransaction(FavoriteFragment())
                 }
                 R.id.action_cart -> {
